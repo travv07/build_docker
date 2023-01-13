e5c8f0b111ca9a90909c0ace02e43d6b83737c2a
travv <travv@runsystem.net>
2023-01-13 16:57:37 +0700

    Update sidekiq

---
+++ app/workers/import_data_csv_worker.rb
@@ -0,0 +1,6 @@
+class ImportDataCsvWorker1
+  include Sidekiq::Worker
+
+  def perform
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module RailsToolbox
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoload_paths += %W(#{config.root})
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
